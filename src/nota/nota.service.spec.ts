c738c2425a0b46a7d99b3f6896d45453eadbb788
Danae321 <167385468+Danae321@users.noreply.github.com>
2025-04-05 10:58:07 -0400

    laboratorio-INF-732

--- src/nota/nota.service.spec.ts
+++ src/nota/nota.service.spec.ts
@@ -5,6 +5,7 @@ import { Nota } from './nota.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { NotFoundException } from '@nestjs/common';
 
+// MOCKS
 const mockNotaRepository = () => ({
   create: jest.fn(),
   save: jest.fn(),
@@ -28,6 +29,7 @@ describe('NotasService', () => {
   let service: NotasService;
   let repository: MockRepository<Nota>;
 
+  // CONFIGURACIÓN INICIAL
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -43,7 +45,131 @@ describe('NotasService', () => {
     repository = module.get<MockRepository<Nota>>(getRepositoryToken(Nota));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  // CREAR NOTA
+  it('debería crear una nota', async () => {
+    jest.spyOn(repository, 'save').mockResolvedValue(mockNota as Nota);
+
+    const result = await service.create({
+      title: 'Test Nota',
+      content: 'Test Content',
+    });
+
+    expect(result).toEqual(mockNota);
+    expect(repository.save).toHaveBeenCalled();
+    expect(repository.create).toHaveBeenCalled();
+  });
+
+  // MOSTRAR TODAS LAS NOTAS
+  it('debería encontrar todas las notas', async () => {
+    jest.spyOn(repository, 'find').mockResolvedValue([mockNota] as Nota[]);
+
+    const result = await service.findAll();
+
+    expect(result).toEqual([mockNota]);
+    expect(repository.find).toHaveBeenCalled();
+  });
+
+  // BUSCAR UNA NOTA
+  describe('select (buscar una nota)', () => {
+    describe('cuando la nota existe', () => {
+      it('debería encontrar una nota por id', async () => {
+        jest.spyOn(repository, 'findOneBy').mockResolvedValue(mockNota as Nota);
+
+        const id: number = 1;
+        const result = await service.findOne(id);
+
+        expect(result).toEqual(mockNota);
+        expect(repository.findOneBy).toHaveBeenCalledWith({ id: id });
+      });
+    });
+
+    describe('cuando la nota no existe', () => {
+      it('debería lanzar un error si no encuentra una nota por id', async () => {
+        jest.spyOn(repository, 'findOneBy').mockResolvedValue(null);
+
+        const id: number = 999;
+
+        await expect(service.findOne(id)).rejects.toThrow(NotFoundException);
+        expect(repository.findOneBy).toHaveBeenCalledWith({ id: id });
+      });
+    });
+  });
+
+  // MODIFICAR NOTA
+  describe('update (modificar una nota)', () => {
+    describe('cuando la nota existe', () => {
+      it('debería modificar una nota', async () => {
+        const id = 1;
+        const updateNotaDto = {
+          title: 'Updated Nota',
+        };
+        const notaActualizada = {
+          ...mockNota,
+          ...updateNotaDto,
+        } as Nota;
+
+        const updateResult = {
+          affected: 1,
+          raw: {},
+          generatedMaps: [],
+        } as UpdateResult;
+
+        jest.spyOn(repository, 'update').mockResolvedValue(updateResult);
+        jest.spyOn(service, 'findOne').mockResolvedValue(notaActualizada);
+
+        const result = await service.update(id, updateNotaDto);
+
+        expect(repository.update).toHaveBeenCalledWith(id, updateNotaDto);
+        expect(service.findOne).toHaveBeenCalledWith(id);
+        expect(result).toEqual(notaActualizada);
+      });
+    });
+
+    describe('cuando la nota no existe', () => {
+      it('debería lanzar NotFoundException si la nota no existe', async () => {
+        const id = 999;
+        const updateNotaDto = {
+          title: 'Updated Nota',
+        };
+
+        const updateResult = {
+          affected: 0,
+          raw: {},
+          generatedMaps: [],
+        } as UpdateResult;
+
+        jest.spyOn(repository, 'update').mockResolvedValue(updateResult);
+        jest.spyOn(service, 'findOne').mockImplementation();
+
+        await expect(service.update(id, updateNotaDto)).rejects.toThrow(NotFoundException);
+        expect(repository.update).toHaveBeenCalledWith(id, updateNotaDto);
+        expect(service.findOne).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  // ELIMINAR NOTA
+  describe('eliminar nota', () => {
+    describe('cuando la nota existe', () => {
+      it('debería eliminar la nota', async () => {
+        const id = 1;
+
+        jest.spyOn(repository, 'delete').mockResolvedValue({ affected: 1 });
+
+        await service.remove(id);
+        expect(repository.delete).toHaveBeenCalledWith(id);
+      });
+    });
+
+    describe('cuando la nota no existe', () => {
+      it('debería lanzar NotFoundException si la nota no existe', async () => {
+        const id = 999;
+
+        jest.spyOn(repository, 'delete').mockResolvedValue({ affected: 0 });
+
+        await expect(service.remove(id)).rejects.toThrow(NotFoundException);
+        expect(repository.delete).toHaveBeenCalledWith(id);
+      });
+    });
   });
 });
